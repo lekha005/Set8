1a396b82c33f55f26f12f954c63fd8585ee9b128
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 05:47:06 +0000

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class fact
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+               System.out.print(i+" ");              
+        }
+}
+}
+
+
+
+
+
+
