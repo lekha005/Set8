12d47540d4341b4ffdb67dfd1795beb65804281c
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 05:05:28 +0000

    Create Flo.java

---
+++ Flo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Flo
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        float n=in.nextFloat();
+        System.out.print((int)n+1);
+}
+}
+
+
+
+
+
+
