3c0e66c7999e8d8fb618e19adcce90fa43170a1f
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 05:49:13 +0000

    Create 13.java

---
+++ 13.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class 13
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+            if(n%13==0)
+               System.out.print("yes");    
+               else
+               System.out.print("no");
+}
+}
+
+
+
+
+
+
